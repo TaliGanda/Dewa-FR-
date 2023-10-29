31892ae8f4520ada80b168861edd2ca6d645dcf3
TaliGanda <145276291+TaliGanda@users.noreply.github.com>
2023-10-29 11:56:56 +0700

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+
+os.system('apt-get install nodejs -y ')
+os.system('apt-get install screen -y ')
+os.system('apt-get install unzip -y ')
+os.system('apt-get install perl -y')
+os.system('apt-get install golang -y')
+os.system('apt install npm')
+os.system('pip install colorama')
+os.system('pip install requests')
+os.system('apt-get install python3 -y')
+os.system('pip install undetected_chromedriver')
+os.system('npm install fs')
+os.system('npm install fake-useragent')
+os.system('npm install url')
+os.system('npm install net')
+os.system('npm install eval')
+os.system('npm install request')
+os.system('npm install randomstring')
+os.system('npm install randomstring')
+os.system('npm install cloudscraper')
+os.system('unzip node_modules.zip')
+os.system('python3 proxy.py')
+os.system('chmod +x *')
+print(f"INSTALL SELESAI") 
+os.system('python3 cnc.py')
